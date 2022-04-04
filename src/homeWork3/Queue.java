05ee924288de7126e6cda4cdc8ac6f2383b0d324
Sergey <sergey_stolyarov@mail.ru>
2022-04-04 13:27:20 +0300

    HomeWork3

---
+++ src/homeWork3/Queue.java
@@ -0,0 +1,18 @@
+package homeWork3;
+
+public interface Queue<E> {
+    boolean insert(E value);
+
+    E remove();
+
+    E peekFront();
+
+    int size();
+
+    boolean isEmpty();
+
+    boolean isFull();
+
+    void display();
+
+}
